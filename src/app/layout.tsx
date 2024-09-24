a0f914e8cf0aced69de59953a85a77f32f9dc2b9
LionStoreTeam <lionstoretechnologyteam@gmail.com>
2024-09-24 17:33:45 -0600

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+
+})
+
+
+export const metadata: Metadata = {
+  title: "Portfolio Cesar Tovar Avilez",
+  description: "Portfolio Cesar Tovar Avilez",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={inter.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
